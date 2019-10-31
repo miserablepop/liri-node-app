05dfe2f63e52e46e8666cd88205a4dcda7b2c102
JC Lenz <jc@miserablepop.com>
2019-10-30 18:00:24 -0700

    Added loop to iterate throw JSON response from Bandsintown. Also added Moment.js package for date formatting

--- liri.js
+++ liri.js
@@ -3,19 +3,35 @@ require("dotenv").config();
 // Importing Spotify keys
 var keys = require('./keys.js');
 
-// Access Spotify keys
-var spotify = new Spotify(keys.spotify);
+// // Access Spotify keys
+// var spotify = new Spotify(keys.spotify);
 
 // Include the axios npm package
 var axios = require('axios');
 
+// Include the moment npm package
+var moment = require('moment');
+
 var command = process.argv[2];
 var searchValue = process.argv[3];
 
+var i, j, x;
+var concerts;
+
 var concertThis = function(artist){
-    axios.get('https://rest.bandsintown.com/artists/' + artist + 'events?').then(
+    axios.get('https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp').then(
         function(response){
-            console.log(response);
+            // console.log(response.data);
+
+            for(i in response.data){
+                var concertDate = response.data[i].datetime;
+                var venue = response.data[i].venue;
+                console.log('--------------------------');
+                console.log('Venue name: ' + venue.name);
+                console.log('Venue location: ' + venue.city + ', ' + venue.region + ' ' + venue.country);
+                console.log('Concert date: ' + moment(concertDate).format('MMMM Do YYYY'));
+            }
+
         }).catch(function(error){
             if (error.response){
                 console.log("---------------Data---------------");
@@ -32,6 +48,7 @@ var concertThis = function(artist){
             console.log(error.config);
     });
 }
+
 
 
 switch (command){
